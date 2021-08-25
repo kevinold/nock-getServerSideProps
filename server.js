c2826974d459a37a5aabe48e4b8fc8b6abd9d8cc
Kevin Old <kevin@cypress.io>
2021-08-25 10:23:22 -0500

    call nock restore, cleanAll and activate before setting nock

--- server.js
+++ server.js
@@ -13,21 +13,27 @@ app.prepare().then(() => {
 
   server.use(express.json());
 
-  server.post("/nock", (req, res) => {
+  server.post("/mock", (req, res) => {
+    nock.restore()
+    nock.cleanAll()
+    nock.activate()
     const { hostname, method, path, statusCode, body } = req.body
     lcMethod = method.toLowerCase()
     nock(hostname)[lcMethod](path).reply(statusCode, body)
     res.sendStatus(200);
   });
 
-  server.get("/clearNock", (req, res) => {
+  server.get('/clearNock', (req, res) => {
     nock.restore()
     nock.cleanAll()
     nock.activate()
     res.sendStatus(200);
-  });
+  })
 
   server.get('*', (req, res) => {
+    nock.restore()
+    nock.cleanAll()
+    nock.activate()
     return handle(req, res)
   })
 
