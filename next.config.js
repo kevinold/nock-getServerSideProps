014f59b5d4a632a10af6d3e19744cb128d6dcf84
Kevin Old <kevin@cypress.io>
2021-09-23 15:07:59 -0500

    attempt to setup webpack devserver config for cypressMockMiddleware

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+//const { cypressMockMiddleware } = require('@cypress/mock-ssr')
+
 module.exports = {
   reactStrictMode: true,
+  /*
+  webpack: (config, options) => {
+    //if (config.name === "server") {
+      config.devServer = {
+        onBeforeSetupMiddleware: (devServer) => {
+          //devServer.app.use(cypressMockMiddleware())
+          devServer.app.get('/kevin', function (req, res) {
+            res.json({ custom: 'response' });
+          });
+        }
+      }
+      //console.log(config)
+    //}
+    return config;
+  },
+  */
 }
